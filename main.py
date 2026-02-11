7e698001db83b1f40a2f30e0b9c20e0bca8e31b6
garvsah74 <garvsah527@gmail.com>
2026-02-11 13:17:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+m= float(input("enter your first number:"))
+n=float(input("enter the second number:"))
+a=m+n
+b=m-n
+c=m*n
+d=m/n
+print (f"addition: {a}")
+print (f"subtraction: {b}")
+print (f"multiplication: {c}")
+print (f"division: {d}")
+

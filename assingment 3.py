551899e0d1701b54ab6ea4567facfdb5305ee28a
garvsah74 <garvsah527@gmail.com>
2026-02-24 12:20:46 +0530

    Add files via upload

---
+++ assingment 3.py
@@ -0,0 +1,10 @@
+
+def factorial(num):
+
+    if num == 1:
+     return 1
+    else:
+       return num*factorial(num-1)
+num=int(input(" enter the number "))
+result=factorial(num)
+print(f"factorial of {num} is : {result}")
